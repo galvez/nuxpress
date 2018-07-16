ac3760e869a4bced416a91ec2a88700e4d19f633
Jonas Galvez <jonasgalvez@gmail.com>
2018-07-16 15:54:06 -0300

    Added head.js

---
+++ head.js
@@ -0,0 +1,12 @@
+
+export default {
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1' },
+    { rel: 'alternate', type: 'application/atom+xml', href: 'http://hire.jonasgalvez.com.br/feed.xml' }
+  ],
+  link: [
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Old+Standard+TT' },
+    { rel: 'stylesheet', href: 'https://code.cdn.mozilla.net/fonts/fira.css' }
+  ]
+}
